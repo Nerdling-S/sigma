118b8a5bc868e7f536276da177a05f4c7138a15e
Nerdling_S <nerdlings9@gmail.com>
2020-10-10 17:06:37 +0200

    echo function for testing

--- sigma.py
+++ sigma.py
@@ -4,7 +4,6 @@ while True:
 
     # quit by whatever means suitable
     if cmd in ['quit', 'exit', 'q', 'bye', 'voetsak']:
-        break
-    # echo with common misspellings
+        break    # echo with common misspellings
     elif cmd[:4] in ['echo', 'ehco', 'echp']:
         print(cmd[5:])
