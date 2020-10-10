342a15ee227cd7d83a9d46033ec5ce77e22b86dc
Nerdling_S <nerdlings9@gmail.com>
2020-10-10 17:06:15 +0200

    infinite loop: accepting cmds until quit

--- sigma.py
+++ sigma.py
@@ -1,2 +1,10 @@
-#Cyan Sigma
-cmd = input("\u001b[36mΣ\u001b[0m ")
+while True:
+    # prompt indicator
+    cmd = input("\u001b[36mΣ\u001b[0m ")
+
+    # quit by whatever means suitable
+    if cmd in ['quit', 'exit', 'q', 'bye', 'voetsak']:
+        break
+    # echo with common misspellings
+    elif cmd[:4] in ['echo', 'ehco', 'echp']:
+        print(cmd[5:])
